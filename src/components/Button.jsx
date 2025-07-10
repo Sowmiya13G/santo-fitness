40997ae0eda3ffd03381feb2e1b746fbadcab8ec
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2025-07-10 12:26:52 +0530

    button update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,23 +8,40 @@ const Button = ({
   iconPosition = "prefix",
   variant = "primary",
   customClassName = "",
+  disabled = false,
+  loading = false,
 }) => {
   const isPrimary = variant === "primary";
 
   const baseStyles =
-    "px-6 py-3 rounded-full font-semibold shadow-md w-full flex items-center justify-center gap-2 h-16 focus:outline-none focus:ring-0 hover:outline-none active:outline-none";
+    "px-6 py-3 rounded-full font-semibold shadow-md w-full flex items-center justify-center gap-2 h-16 focus:outline-none focus:ring-0 hover:outline-none active:outline-none transition-opacity duration-300";
 
   const buttonClass = `${baseStyles} ${
     isPrimary ? "bg-primary-gradient" : "bg-white"
-  } ${customClassName}`;
+  } ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""} ${customClassName}`;
 
   return (
-    <button type={type} onClick={onClick} className={buttonClass}>
-      {icon && iconPosition === "prefix" && <span>{icon}</span>}
-      <span className={`${isPrimary ? "text-white" : "text-gradient"}`}>
-        {children}
-      </span>
-      {icon && iconPosition === "suffix" && <span>{icon}</span>}
+    <button
+      type={type}
+      onClick={onClick}
+      className={buttonClass}
+      disabled={disabled || loading}
+    >
+      {loading ? (
+        <span className="flex gap-1 items-center">
+          <span className="dot animate-bounce delay-0">.</span>
+          <span className="dot animate-bounce delay-150">.</span>
+          <span className="dot animate-bounce delay-300">.</span>
+        </span>
+      ) : (
+        <>
+          {icon && iconPosition === "prefix" && <span>{icon}</span>}
+          <span className={`${isPrimary ? "text-white" : "text-gradient"}`}>
+            {children}
+          </span>
+          {icon && iconPosition === "suffix" && <span>{icon}</span>}
+        </>
+      )}
     </button>
   );
 };
