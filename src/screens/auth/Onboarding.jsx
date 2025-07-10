d3d2e7a6ff39c07863b819673e70ae140c1a8be8
gokul1818 <gokulakrishnan.developer18@gmail.com>
2025-07-10 16:59:13 +0530

    udpate changes

--- src/screens/auth/Onboarding.jsx
+++ src/screens/auth/Onboarding.jsx
@@ -23,7 +23,7 @@ export default function Onboarding({ onComplete }) {
   };
 
   return (
-    <div className="h-full w-screen flex flex-col justify-between bg-white">
+    <div className="h-[100vh] w-screen flex flex-col justify-between bg-white">
       <img
         src={image}
         alt={title}
--- src/screens/auth/Splash.jsx
+++ src/screens/auth/Splash.jsx
@@ -39,14 +39,14 @@ const SplashScreen = () => {
 
   return (
     <div className="h-[100vh] overflow-hidden w-screen fixed inset-0 bg-primary-gradient text-white flex flex-col items-center justify-between p-8 z-50">
-      <div className="flex flex-col items-center justify-center mt-24">
+      <div className="flex flex-col items-center justify-center mt-2">
         <img
           src={logo}
           alt="App Logo"
           className="w-50 h-50 mb-6 object-contain"
         />
-        <h2 className="text-4xl font-bold font-poppin">Santo Fitness</h2>
-        <h2 className="text-xl text-bg_primary font-poppin mt-5">
+        <h2 className="text-4xl font-bold ">Santo Fitness</h2>
+        <h2 className="text-xl text-bg_primary  mt-5">
           Everybody Can Train
         </h2>
       </div>
