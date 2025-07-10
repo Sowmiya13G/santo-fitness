80e87be384a7cd81ce5e2583d01b19d649bfba87
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2025-07-10 12:34:57 +0530

    fixed

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -14,6 +14,7 @@ import Profile from "../screens/dashboard/Profile";
 import Recipes from "../screens/dashboard/Recipes";
 import DashboardLayout from "./DashboardLayout";
 
+
 export default function AppRoutes() {
   const { isLoggedIn } = useAuth();
 
