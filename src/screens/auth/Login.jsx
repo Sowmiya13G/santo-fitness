fe8e4d0f40202a94404214f4025e785558ec4bf2
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2025-07-10 16:15:52 +0530

    ui fix

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -76,7 +76,7 @@ function Login() {
   // ---------------------------------- render ui ---------------------------------- //
 
   return (
-    <AuthLayout>
+    <div className="min-h-screen w-screen bg-white flex flex-col items-center px-6 justify-center font-poppin">
       <div className="w-full max-w-md text-center  ">
         <p className="text-font_primary text-base mb-2">Hey there,</p>
         <p className="text-font_primary font-bold text-xl">Welcome back</p>
@@ -122,7 +122,7 @@ function Login() {
           </div>
         </form>
       </FormProvider>
-    </AuthLayout>
+    </div>
   );
 }
 
