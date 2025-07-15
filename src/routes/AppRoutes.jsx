e0f8c80e2717434c3a0d60ab3f85207931146da5
Sowmiya13G <sowmiyag13.doodleblue@gmail.com>
2025-07-15 21:40:10 +0530

    fixes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,6 +10,7 @@ import Onboarding from "@/screens/auth/oboarding";
 import SetPassword from "@/screens/auth/set-password";
 import SplashScreen from "@/screens/auth/splash";
 
+
 import Profile from "@/screens/dashboard/Profile";
 import Camera from "@/screens/dashboard/camera";
 import Graph from "@/screens/dashboard/graph";
