c287711306c1cd9f253edb57469cfc99b4977f48
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2025-08-10 12:23:01 +0530

    da

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
 import { queryClient } from "./app/query-client";
 import { persistor, store } from "./features/store.js";
